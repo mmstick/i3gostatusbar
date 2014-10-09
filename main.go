7a50994fce681dbab51f17cdb38f9dc2371a8f9f
mmstick <mmstickman@gmail.com>
2014-10-09 09:52:00 -0500

    Fix CPU Temp Format

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ var refreshRate = 1 * time.Second
 // If a battery exists, this will return an additional string element.
 func statusBarFormat(batteryExists bool) string {
 	if batteryExists {
-		return "%s@%s | %s | %s | %s %s | %s | Temp: %sC | %s %s %s | %s | %s\n"
+		return "%s@%s | %s | %s | %s %s | %s | Temp: %dC | %s %s %s | %s | %s\n"
 	} else {
-		return "%s@%s | %s | %s | %s %s | %s | Temp: %sC | %s %s %s | %s\n"
+		return "%s@%s | %s | %s | %s %s | %s | Temp: %dC | %s %s %s | %s\n"
 	}
 }
 
