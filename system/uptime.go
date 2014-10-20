7665e2732f6d925724650652aecc3ff45b18a542
mmstick <mmstickman@gmail.com>
2014-10-20 18:21:41 -0500

    Fix bugs caused by last push

--- system/uptime.go
+++ system/uptime.go
@@ -4,7 +4,6 @@ package system
 import (
 	"fmt"
 	"io/ioutil"
-	"strconv"
 	"strings"
 )
 
@@ -12,39 +11,39 @@ import (
 // (days/hours/minutes). After determining the amount of time in that scale,
 // it subtracts that amount from uptime and returns the time in
 // days/hours/minutes.
-func getTimeScale(time **int, scale int) int {
+func getTimeScale(time *int, scale int) int {
 	var output int
-	if **time > scale {
-		output = **time / scale
-		**time -= output * scale
+	if *time > scale {
+		output = *time / scale
+		*time -= output * scale
 	}
 	return output
 }
 
 // getDays returns the time formatted in days and subtracts that from time.
-func getDays(time *int) string {
-	return getTimeScale(&time, 86400)
+func getDays(time *int) int {
+	return getTimeScale(time, 86400)
 }
 
 // getHours returns the time formatted in hours and subtracts that from time.
-func getHours(time *int) string {
-	return getTimeScale(&time, 3600)
+func getHours(time *int) int {
+	return getTimeScale(time, 3600)
 }
 
 // getMinutes returns the time formatted in minutes and subtracts that from time.
-func getMinutes(time *int) string {
-	return getTimeScale(&time, 60)
+func getMinutes(time *int) int {
+	return getTimeScale(time, 60)
 }
 
 // getSeconds returns the time formatted in seconds.
-func getSeconds(time *int) string {
+func getSeconds(time *int) int {
 	return *time
 }
 
 // humanReadableTime takes the uptime integer and converts it into a human
 // readable format. Ex: 01:21:18:57 for days:hours:seconds:minutes
 func humanReadableTime(time int) string {
-	return fmt.Sprintf("%0.2s:%0.2s:%0.2s:%0.2s", getDays(&time),
+	return fmt.Sprintf("%0.2d:%0.2d:%0.2d:%0.2d", getDays(&time),
 		getHours(&time), getMinutes(&time), getSeconds(&time))
 }
 
