7669ad83d9a99ef36cde277a3140226d75c8e6ed
mmstick <mmstickman@gmail.com>
2014-10-20 18:17:41 -0500

    Reduce SLOC in Uptime No need to manually pad digits when using printf.

--- system/uptime.go
+++ system/uptime.go
@@ -21,40 +21,31 @@ func getTimeScale(time **int, scale int) int {
 	return output
 }
 
-// padTime an extra zero in case the time only has one digit
-func padTime(time int) string {
-	output := strconv.Itoa(time)
-	if len(output) == 1 {
-		output = "0" + output
-	}
-	return output
-}
-
 // getDays returns the time formatted in days and subtracts that from time.
 func getDays(time *int) string {
-	return padTime(getTimeScale(&time, 86400))
+	return getTimeScale(&time, 86400)
 }
 
 // getHours returns the time formatted in hours and subtracts that from time.
 func getHours(time *int) string {
-	return padTime(getTimeScale(&time, 3600))
+	return getTimeScale(&time, 3600)
 }
 
 // getMinutes returns the time formatted in minutes and subtracts that from time.
 func getMinutes(time *int) string {
-	return padTime(getTimeScale(&time, 60))
+	return getTimeScale(&time, 60)
 }
 
 // getSeconds returns the time formatted in seconds.
 func getSeconds(time *int) string {
-	return padTime(*time)
+	return *time
 }
 
 // humanReadableTime takes the uptime integer and converts it into a human
 // readable format. Ex: 01:21:18:57 for days:hours:seconds:minutes
 func humanReadableTime(time int) string {
-	return fmt.Sprintf("%s:%s:%s:%s", getDays(&time), getHours(&time),
-		getMinutes(&time), getSeconds(&time))
+	return fmt.Sprintf("%0.2s:%0.2s:%0.2s:%0.2s", getDays(&time),
+		getHours(&time), getMinutes(&time), getSeconds(&time))
 }
 
 // parseUptime opens /proc/uptime and return the value in integer format.
